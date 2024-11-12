d9d6feb03967fd6379df2ae2f6896858d4e4fd28
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2024-11-12 15:32:11 +0100

    changed test string

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from ursina import *
 if __name__ == "__main__":
     app = Ursina()
     from solver import MainPage
-    MainPage("R U R F' B' L' D2 B2 U2 L2 D2 R2 U2 F2 L2 D2 B2 U2 R2")
+    MainPage("R U R F' B' L' D2 R2")
     app.run()
